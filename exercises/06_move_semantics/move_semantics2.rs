9df4c0063719a7acc8e5a233c5d8cccbc8b324e4
alex <alexgrimes.se@gmail.com>
2025-02-16 11:33:40 -0500

    progress on move semantics

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -7,22 +7,18 @@
 
 #[test]
 fn main() {
-    let vec0 = Vec::new();
+    let vec0 = vec![22, 44, 66];
 
-    let mut vec1 = fill_vec(&vec0);
-
-    vec1.push(88);
+    let vec1 = fill_vec(&vec0);
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
 }
 
 fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
-    let mut vec = vec.clone;
+    let mut vec = vec.clone();
 
-    vec.push(22);
-    vec.push(44);
-    vec.push(66);
+    vec.push(88);
 
     vec
 }
